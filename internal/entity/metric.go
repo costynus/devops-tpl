44fe4a760f8008354e7257d2342325d097dbf1ef
Konstantin <konstantin.nabatchikov@mysky.com>
2022-06-15 12:27:53 +0300

    update/ and value/ with json req/resp

--- internal/entity/metric.go
+++ internal/entity/metric.go
@@ -13,6 +13,12 @@ type (
 		Name  string
 		Value interface{}
 	}
+	Metrics struct {
+		ID    string   `json:"id"`              // имя метрики
+		MType string   `json:"type"`            // параметр, принимающий значение gauge или counter
+		Delta *Counter `json:"delta,omitempty"` // значение метрики в случае передачи counter
+		Value *Gauge   `json:"value,omitempty"` // значение метрики в случае передачи gauge
+	}
 )
 
 func ParseGauge(value string) (Gauge, error) {
