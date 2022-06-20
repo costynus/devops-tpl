32a037f72404a4a16df98104733b0c3dac5252cc
Konstantin <konstantin.nabatchikov@mysky.com>
2022-06-20 14:35:03 +0300

    refactoring config

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"devops-tpl/config"
 	"devops-tpl/internal/app/agent"
+	"flag"
 
 	"log"
 )
 
 func main() {
-	cfg, err := config.NewConfig()
+	cfg := config.NewConfig()
+	// Flag init -.
+	flag.StringVar(&cfg.Agent.ServerURL, "a", cfg.Agent.ServerURL, "server address")
+	flag.DurationVar(&cfg.Agent.ReportInterval, "r", cfg.Agent.ReportInterval, "report interval")
+	flag.DurationVar(&cfg.Agent.PollInterval, "p", cfg.Agent.PollInterval, "poll interval")
+	err := config.Init(cfg)
 	if err != nil {
 		log.Fatalf("Config error: %s", err)
 	}
--- config/config.go
+++ config/config.go
@@ -35,28 +35,24 @@ type (
 	}
 )
 
-func NewConfig() (*Config, error) {
+func NewConfig() *Config {
 	cfg := &Config{}
+	return cfg
+}
 
+func Init(cfg *Config) error {
 	// YAML Config -.
 	err := cleanenv.ReadConfig("./config/config.yml", cfg)
 	if err != nil {
-		return nil, fmt.Errorf("config error: %w", err)
+		return fmt.Errorf("config error: %w", err)
 	}
 
 	// Flags Config -.
-	flag.StringVar(&cfg.Server.Address, "a", cfg.Server.Address, "address to listen on")
-	flag.BoolVar(&cfg.Server.Restore, "r", cfg.Server.Restore, "restore data from file")
-	flag.DurationVar(&cfg.Server.StoreInterval, "i", cfg.Server.StoreInterval, "store interval")
-	flag.StringVar(&cfg.Server.StoreFile, "f", cfg.Server.StoreFile, "store file")
-
-	flag.StringVar(&cfg.Agent.ServerURL, "a", cfg.Agent.ServerURL, "server address")
-	flag.DurationVar(&cfg.Agent.ReportInterval, "r", cfg.Agent.ReportInterval, "report interval")
-	flag.DurationVar(&cfg.Agent.PollInterval, "p", cfg.Agent.PollInterval, "poll interval")
+	flag.Parse()
 
 	// Env config -.
 	if err = cleanenv.ReadEnv(cfg); err != nil {
-		return nil, fmt.Errorf("config error: %w", err)
+		return fmt.Errorf("config error: %w", err)
 	}
-	return cfg, nil
+	return nil
 }
