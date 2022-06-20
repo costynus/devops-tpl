9cebce7144917cfb6c9c2fd5ee07beb00341eebd
Konstantin <konstantin.nabatchikov@mysky.com>
2022-06-20 19:52:12 +0300

    added read gzip

--- internal/app/server/middleware.go
+++ internal/app/server/middleware.go
@@ -18,7 +18,17 @@ func (w gzipWriter) Write(b []byte) (int, error) {
 
 func gzipReadHandle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO: pass
+		if !strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+			next.ServeHTTP(w, r)
+			return
+		}
+		gz, err := gzip.NewReader(r.Body)
+		if err != nil {
+			errorHandler(w, err)
+		}
+		defer gz.Close()
+
+		r.Body = gz
 		next.ServeHTTP(w, r)
 	})
 }
