a74f06d44bb32ce7cc8a75f5c433eeb32b82a904
Konstantin <konstantin.nabatchikov@mysky.com>
2022-06-24 17:49:34 +0300

    refactoring router

--- internal/app/server/router.go
+++ internal/app/server/router.go
@@ -29,179 +29,186 @@ func NewRouter(handler *chi.Mux, uc usecase.DevOps, l logger.Interface) {
 	handler.Use(gzipWriteHandle)
 
 	// checker
-	handler.Get("/healthz", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusOK) })
+	handler.Get("/healthz", healthzHandler())
 
-	handler.Get(
-		"/",
-		func(w http.ResponseWriter, r *http.Request) {
-			names, err := uc.GetMetricNames(context.TODO())
-			if err != nil {
-				errorHandler(w, err)
-				return
-			}
-
-			w.Header().Set("Content-Type", "text/html")
-			w.Write([]byte(strings.Join(names, "\n")))
-		})
+	handler.Get("/", getMetricNamesHandler(uc, l))
 
 	// updater
 	handler.Route("/update", func(r chi.Router) {
-		r.Post(
-			"/",
-			func(w http.ResponseWriter, r *http.Request) {
-				var metric entity.Metric
-
-				if err := json.NewDecoder(r.Body).Decode(&metric); err != nil {
-					http.Error(w, "bad request", http.StatusBadRequest)
-					return
-				}
-
-				err := uc.StoreMetric(context.TODO(), metric)
-				if err != nil {
-					l.Error(err)
-					errorHandler(w, err)
-					return
-				}
-				w.Header().Set("Content-Type", "text/plain")
-				w.WriteHeader(http.StatusOK)
-			},
-		)
-		r.Post(
-			"/gauge/{metricName}/{metricValue}",
-			func(w http.ResponseWriter, r *http.Request) {
-				value, err := entity.ParseGauge(chi.URLParam(r, "metricValue"))
-				if err != nil {
-					l.Error(err)
-					http.Error(w, "parsing error", http.StatusBadRequest)
-					return
-				}
-
-				metric := entity.Metric{
-					ID:    chi.URLParam(r, "metricName"),
-					MType: value.String(),
-					Value: &value,
-				}
-
-				err = uc.StoreMetric(context.TODO(), metric)
-				if err != nil {
-					l.Error(err)
-					errorHandler(w, err)
-					return
-				}
-
-				w.Header().Set("Content-Type", "text/plain")
-				w.WriteHeader(http.StatusOK)
-			},
-		)
-		r.Post(
-			"/counter/{metricName}/{metricValue}",
-			func(w http.ResponseWriter, r *http.Request) {
-				value, err := entity.ParseCounter(chi.URLParam(r, "metricValue"))
-				if err != nil {
-					l.Error(err)
-					http.Error(w, "parsing error", http.StatusBadRequest)
-					return
-				}
-
-				metric := entity.Metric{
-					ID:    chi.URLParam(r, "metricName"),
-					MType: value.String(),
-					Delta: &value,
-				}
-
-				err = uc.StoreMetric(context.TODO(), metric)
-				if err != nil {
-					l.Error(err)
-					errorHandler(w, err)
-					return
-				}
-
-				w.Header().Set("Content-Type", "text/plain")
-				w.WriteHeader(http.StatusOK)
-			},
-		)
-		r.Post(
-			"/{metricType}/{metricName}/{metricValue}",
-			func(w http.ResponseWriter, r *http.Request) {
-				w.Header().Set("Content-Type", "text/plain")
-				http.Error(w, "not implemented", http.StatusNotImplemented)
-			},
-		)
+		r.Post("/", updateMetric(uc, l))
+		r.Post("/gauge/{metricName}/{metricValue}", updateGaugeMetric(uc, l))
+		r.Post("/counter/{metricName}/{metricValue}", updateCounterMetric(uc, l))
+		r.Post("/{metricType}/{metricName}/{metricValue}", notImplemented())
 	})
 
 	// value
 	handler.Route("/value", func(r chi.Router) {
-		r.Post(
-			"/",
-			func(w http.ResponseWriter, r *http.Request) {
-				var metric entity.Metric
-
-				if err := json.NewDecoder(r.Body).Decode(&metric); err != nil {
-					http.Error(w, "bad request", http.StatusBadRequest)
-					return
-				}
-
-				metric, err := uc.GetMetric(context.TODO(), metric)
-				if err != nil {
-					l.Error(err)
-					errorHandler(w, err)
-					return
-				}
-
-				jsonResp, err := json.Marshal(metric)
-				if err != nil {
-					l.Error(err)
-					errorHandler(w, err)
-					return
-				}
-				w.Header().Set("Content-Type", "application/json")
-				w.Write(jsonResp)
-			},
-		)
-		r.Get(
-			"/gauge/{metricName}",
-			func(w http.ResponseWriter, r *http.Request) {
-				metric := entity.Metric{
-					ID:    chi.URLParam(r, "metricName"),
-					MType: Gauge,
-				}
-
-				metric, err := uc.GetMetric(context.TODO(), metric)
-				if err != nil {
-					l.Error(err)
-					errorHandler(w, err)
-					return
-				}
-
-				w.Header().Set("Content-Type", "text/plain")
-				w.Write([]byte(fmt.Sprintf("%g", *metric.Value)))
-			},
-		)
-		r.Get(
-			"/counter/{metricName}",
-			func(w http.ResponseWriter, r *http.Request) {
-				metric := entity.Metric{
-					ID:    chi.URLParam(r, "metricName"),
-					MType: Counter,
-				}
-
-				metric, err := uc.GetMetric(context.TODO(), metric)
-				if err != nil {
-					l.Error(err)
-					errorHandler(w, err)
-					return
-				}
-
-				w.Header().Set("Content-Type", "text/plain")
-				w.Write([]byte(fmt.Sprintf("%d", *metric.Delta)))
-			},
-		)
-		r.Get(
-			"/{metricType}/{metricName}/{metricValue}",
-			func(w http.ResponseWriter, r *http.Request) {
-				http.Error(w, "not implemented", http.StatusNotImplemented)
-				w.Header().Set("Content-Type", "text/plain")
-			},
-		)
+		r.Post("/", getValue(uc, l))
+		r.Get("/gauge/{metricName}", getGaugeValue(uc, l))
+		r.Get("/counter/{metricName}", getCounterValue(uc, l))
+		r.Get("/{metricType}/{metricName}/{metricValue}", notImplemented())
 	})
+}
+
+func healthzHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusOK) }
+}
+
+func notImplemented() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		http.Error(w, "not implemented", http.StatusNotImplemented)
+	}
+}
+
+func getMetricNamesHandler(uc usecase.DevOps, l logger.Interface) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		names, err := uc.GetMetricNames(context.TODO())
+		if err != nil {
+			errorHandler(w, err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte(strings.Join(names, "\n")))
+	}
+}
+
+func updateMetric(uc usecase.DevOps, l logger.Interface) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var metric entity.Metric
+
+		if err := json.NewDecoder(r.Body).Decode(&metric); err != nil {
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+
+		err := uc.StoreMetric(context.TODO(), metric)
+		if err != nil {
+			l.Error(err)
+			errorHandler(w, err)
+			return
+		}
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func updateGaugeMetric(uc usecase.DevOps, l logger.Interface) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		value, err := entity.ParseGauge(chi.URLParam(r, "metricValue"))
+		if err != nil {
+			l.Error(err)
+			http.Error(w, "parsing error", http.StatusBadRequest)
+			return
+		}
+
+		metric := entity.Metric{
+			ID:    chi.URLParam(r, "metricName"),
+			MType: value.String(),
+			Value: &value,
+		}
+
+		err = uc.StoreMetric(context.TODO(), metric)
+		if err != nil {
+			l.Error(err)
+			errorHandler(w, err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func updateCounterMetric(uc usecase.DevOps, l logger.Interface) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		value, err := entity.ParseCounter(chi.URLParam(r, "metricValue"))
+		if err != nil {
+			l.Error(err)
+			http.Error(w, "parsing error", http.StatusBadRequest)
+			return
+		}
+
+		metric := entity.Metric{
+			ID:    chi.URLParam(r, "metricName"),
+			MType: value.String(),
+			Delta: &value,
+		}
+
+		err = uc.StoreMetric(context.TODO(), metric)
+		if err != nil {
+			l.Error(err)
+			errorHandler(w, err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func getValue(uc usecase.DevOps, l logger.Interface) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var metric entity.Metric
+
+		if err := json.NewDecoder(r.Body).Decode(&metric); err != nil {
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+
+		metric, err := uc.GetMetric(context.TODO(), metric)
+		if err != nil {
+			l.Error(err)
+			errorHandler(w, err)
+			return
+		}
+
+		jsonResp, err := json.Marshal(metric)
+		if err != nil {
+			l.Error(err)
+			errorHandler(w, err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonResp)
+	}
+}
+
+func getGaugeValue(uc usecase.DevOps, l logger.Interface) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		metric := entity.Metric{
+			ID:    chi.URLParam(r, "metricName"),
+			MType: Gauge,
+		}
+
+		metric, err := uc.GetMetric(context.TODO(), metric)
+		if err != nil {
+			l.Error(err)
+			errorHandler(w, err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte(fmt.Sprintf("%g", *metric.Value)))
+	}
+}
+
+func getCounterValue(uc usecase.DevOps, l logger.Interface) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		metric := entity.Metric{
+			ID:    chi.URLParam(r, "metricName"),
+			MType: Counter,
+		}
+
+		metric, err := uc.GetMetric(context.TODO(), metric)
+		if err != nil {
+			l.Error(err)
+			errorHandler(w, err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte(fmt.Sprintf("%d", *metric.Delta)))
+	}
 }
